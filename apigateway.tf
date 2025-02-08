05c9b3da264d5907eab9775fbe098a141de7b05b
jingyang022 <jingyang022@yahoo.com.sg>
2025-02-08 15:53:35 +0800

    fixed errors in backend.tf file

--- apigateway.tf
+++ apigateway.tf
@@ -38,11 +38,10 @@ resource "aws_api_gateway_domain_name" "shortener" {
   domain_name     = "api.yourdomain.com" # Our custom domain
   certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
 
-  endpoint "configuration" {
+  endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
-
 
 resource "aws_api_gateway_base_path_mapping" "shorterner" {
   api_id      = aws_api_gateway_rest_api.api.id
