08bbe36fc38b1ba04106a6011b41f7945d536f53
jingyang022 <jingyang022@yahoo.com.sg>
2025-02-08 11:52:50 +0800

    fix iam.tf file

--- iam.tf
+++ iam.tf
@@ -24,24 +24,3 @@ resource "aws_iam_policy_attachment" "url_dynamoDB_attach" {
   roles      = [aws_iam_role.lambda_exec_role.name]
   policy_arn = aws_iam_policy.url_dynamoDB_access.arn
 }
-
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "url-api-executionrole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Effect = "Allow"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
