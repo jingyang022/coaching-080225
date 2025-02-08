0fcde3132d69aaa11c69caa44019bd70025f1c00
Buckyatplay <rvf_7585@hotmail.com>
2025-02-08 12:18:51 +0800

    cy2

--- apigateway.tf
+++ apigateway.tf
@@ -1,16 +1,25 @@
-resource "aws_acm_certificate" "certificate" {
-  domain_name       = "api.yourdomain.com"  # Our custom domain
+# Step 1: Request an ACM Certificate
+resource "aws_acm_certificate" "mycert_acm" {
+  domain_name       = "yap080225.sctp-sandbox.com" # Our custom domain
   validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-resource "aws_route53_record" "www" {
-  name    = aws_acm_certificate.certificate.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.certificate.domain_validation_options[0].resource_record_type
-  zone_id = "Z00541411T1NGPV97B5C0"  # Our Route 53 hosted zone ID
-  records = [aws_acm_certificate.certificate.domain_validation_options[0].resource_record_value]
-  ttl     = 60
-}
+# Step 2: Create Route 53 DNS Record for Validation
+resource "aws_route53_record" "cert_validation_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.mycert_acm.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
+
+#Step 3: Handle Certificate Validation
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn = aws_acm_certificate.certificate.arn
 
@@ -18,6 +27,25 @@ resource "aws_acm_certificate_validation" "cert_validation" {
     aws_route53_record.cert_validation.fqdn
   ]
 }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.sctp_zone.zone_id
+}
+
+/* Ref from previous exercise
+resource "aws_acm_certificate_validation" "cert_validation" {
+
+  timeouts {
+    create = "5m"
+  }
+  
+  certificate_arn         = aws_acm_certificate.mycert_acm.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_record : record.fqdn]
+}
+*/
 
 resource "aws_api_gateway_rest_api" "api" {
   name        = "MyAPI"
@@ -35,14 +63,13 @@ resource "aws_api_gateway_deployment" "deployment" {
 }
 
 resource "aws_api_gateway_domain_name" "shortener" {
-  domain_name = "api.yourdomain.com"  # Our custom domain
+  domain_name = "yap080225.sctp-sandbox.com"  # Our custom domain
   certificate_arn = aws_acm_certificate_validation.cert_validation.certificate_arn
 
 endpoint configuration {
   types = ["REGIONAL"]
   }
 }
-
 
 resource "aws_api_gateway_base_path_mapping" "shorterner" {
   api_id      = aws_api_gateway_rest_api.api.id
